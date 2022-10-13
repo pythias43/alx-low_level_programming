faba34b93da076e313ac6215dc7f2e0b6a341ae3
pythias43 <pythiasmalaka43@gmail.com>
2022-10-13 11:27:43 -0700

    my commits

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,8 @@
  *
  * Return: Always 0 (Successful)
  */
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+
+
 /**
  * main - Prints the size of the many var types
  *
  * Return:Always 0 (Success)
  */
+
+
+int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
