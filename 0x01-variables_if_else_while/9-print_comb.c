0707db9a9416d9196873991c414f755a6ffd99b4
pythias43 <pythiasmalaka43@gmail.com>
2022-10-14 13:16:34 -0700

    my cmmits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-	int c;
-	for (c = '0'; c <= '9'; c++)
-	{
-		putchar(c);
-		if (c != '9')
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+int c;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+if (c != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
 
 
