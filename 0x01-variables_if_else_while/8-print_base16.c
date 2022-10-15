c097004529827be8b3919e6f020a026cb970d71d
pythias43 <pythiasmalaka43@gmail.com>
2022-10-15 03:05:05 -0700

    my commits

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints a hexadecimal string 
+ * main - Prints a hexadecimal string
  * Return: Always (Success)
  *
  */
@@ -8,15 +8,17 @@
 int main(void)
 {
 	char c;
+
 	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
 	}
 	for (c = 'a'; c <= 'f'; c++)
 	{
-		putchar(c);
+	putchar(c);
 	}
 	putchar('\n');
+
 	return (0);
 }
 
