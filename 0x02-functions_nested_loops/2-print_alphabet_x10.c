ac29597c1fbb8a26f6f7db3386229ebcdba82924
pythias43 <pythiasmalaka43@gmail.com>
2022-10-18 15:59:19 -0700

    second commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print the alphabet 10x -> print the alphabet 10x  in lower case
+ */
+
+void print_alphabet_x10(void)
+{
+	int j;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
