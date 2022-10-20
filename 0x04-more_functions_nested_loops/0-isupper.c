768d1f19a3dfb771203313c7d4a1d439d55b182f
pythias43 <pythiasmalaka43@gmail.com>
2022-10-20 16:27:44 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper - checks for the uppercase character
- * @c: variable text
- * Return: always 0
+ * _isupper - checks if the character is uppercase
+ * @c: Variable text
+ * Return: Always 0
  *
  */
 int _isupper(int c)
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -3,7 +3,7 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c - the charecter to print
+ * @c: the charecter to print
  * Return: on success 1 else -1
  */
 int _putchar(char c)
